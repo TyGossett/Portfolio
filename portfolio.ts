1d0b81beaed606aa0b156b2cb54249281c6d3a88
TyGossett <34064388+TyGossett@users.noreply.github.com>
2023-03-13 00:31:20 -0400

    Last For the night

--- portfolio.ts
+++ portfolio.ts
@@ -15,7 +15,7 @@ export const greetings: GreetingsType = {
   name: 'Ty Gossett',
   title: "Hi all, I'm Ty Gossett",
   description:
-    "I'Experienced data scientist skilled in predictive modeling, machine learning algorithms,and data analysis strategies. Passionate about leveraging statistical analysis, data visualization, and programming to drive revenue growth, cost savings, and process efficiencies for clientsm passionate Full Stack web developer having an experience of web applications with Python, Django, React.js, Next.js and Blockchain development on Ethereum, Solidity, Web3.js, Moralis, and Brownie Framework.",
+    "I'm an experienced data scientist skilled in predictive modeling, machine learning algorithms, and data analysis strategies. Passionate about leveraging statistical analysis, data visualization, and programming to drive revenue growth, cost savings, and process efficiencies for clients. Effective communicator and collaborative team player committed to solving complex business challenges through the power of data science.",
   resumeLink:
     'https://1drv.ms/b/s!AoJRZTfHBX6ngkAqa0JZWPgBbvEb?e=7gyVMq',
 };
@@ -31,12 +31,10 @@ export const socialLinks = {
   linkedin: 'https://www.linkedin.com/in/ty-gossett-254382a5/',
   github: 'https://github.com/TyGossett',
   instagram: 'https://www.instagram.com/gossettty/',
-  facebook: 'https://www.facebook.com/',
-  twitter: 'https://www.twitter.com/'
 };
 
 export const skillsSection: SkillsSectionType = {
-  title: 'Somne of my projects',
+  title: 'Some of my projects',
   subTitle: 'Data Scientist with a passion for work that helps people.',
   data: [
     {
@@ -55,11 +53,11 @@ export const skillsSection: SkillsSectionType = {
         },
         {
           skillName: 'LaTex',
-          fontAwesomeClassName: 'logos:latex-dark',
+          fontAwesomeClassName: 'skill-icons:latex-dark',
         },
         {
           skillName: 'Machine Learning',
-          fontAwesomeClassName: 'logos:machine-learning-model',
+          fontAwesomeClassName: 'carbon:machine-learning-model',
         },
         {
           skillName: 'numpy',
@@ -67,125 +65,104 @@ export const skillsSection: SkillsSectionType = {
         },
         {
           skillName: 'SciKitLearn',
-          fontAwesomeClassName: 'logos:scikitlearn',
+          fontAwesomeClassName: 'simple-icons:scikitlearn',
         },
         {
           skillName: 'Python',
           fontAwesomeClassName: 'logos:python',
-        },
-        {
-          skillName: 'Django',
-          fontAwesomeClassName: 'vscode-icons:file-type-django',
-        },
-
-        {
-          skillName: 'Redux',
-          fontAwesomeClassName: 'logos:redux',
-        },
-        {
-          skillName: 'NPM',
-          fontAwesomeClassName: 'logos:npm-icon',
-        },
-        {
-          skillName: 'Yarn',
-          fontAwesomeClassName: 'logos:yarn',
-        },
+        }
       ],
     },
     {
-      title: 'Cloud Infra-Architecture',
-      lottieAnimationFile: '/lottie/skills/cloudinfra.json', // Path of Lottie Animation JSON File
+      title: 'Data-Strategy',
+      lottieAnimationFile: '/lottie/skills/DataStrategy.json', // Path of Lottie Animation JSON File
       skills: [
-        emoji('⚡ Experience of working on multiple cloud platforms'),
-        emoji(
-          '⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases'
-        ),
-        emoji(
-          '⚡ Building CI/CD pipelines for automated testing & deployment using Github Actions'
-        ),
+        emoji('⚡ Developed and implemented a cross-functional initiative for New York based medical staffing firm to develop a comprehensive data strategy, working closely with stakeholders to ensure data privacy, integrity, and quality.'),
+        emoji('⚡ Successfully implemented a data model that better aligned their database with their ongoing business and marketing objectives.'),
       ],
       softwareSkills: [
         // ? Check README To get icon details
         {
-          skillName: 'AWS',
-          fontAwesomeClassName: 'logos:aws',
+          skillName: 'HubSpot',
+          fontAwesomeClassName: 'logos:hubspot',
         },
         {
           skillName: 'Azure',
           fontAwesomeClassName: 'logos:microsoft-azure',
         },
         {
-          skillName: 'Heroku',
-          fontAwesomeClassName: 'logos:heroku-icon',
+          skillName: 'AWS',
+          fontAwesomeClassName: 'logos:aws',
         },
         {
-          skillName: 'PostgreSQL',
-          fontAwesomeClassName: 'logos:postgresql',
+          skillName: 'SQL',
+          fontAwesomeClassName: 'logos:mysql',
         },
         {
-          skillName: 'Github',
-          fontAwesomeClassName: 'akar-icons:github-fill',
-        },
-        {
-          skillName: 'Docker',
-          fontAwesomeClassName: 'logos:docker-icon',
-        },
-        {
-          skillName: 'Github Actions',
-          fontAwesomeClassName: 'logos:github-actions',
-        },
-        {
-          skillName: 'Cloudinary',
-          fontAwesomeClassName: 'logos:cloudinary',
-        },
-        {
-          skillName: 'Nginx',
-          fontAwesomeClassName: 'logos:nginx',
-        },
-        {
-          skillName: 'Sentry',
-          fontAwesomeClassName: 'logos:sentry-icon',
+          skillName: 'Client training',
+          fontAwesomeClassName: 'material-symbols:on-device-training',
         },
       ],
     },
     {
-      title: 'Blockchain',
-      lottieAnimationFile: '/lottie/skills/ethereum.json', // Path of Lottie Animation JSON File
+      title: 'Employee Satisfaction Survey',
+      lottieAnimationFile: '/lottie/skills/EmployeSatSurvey.json', // Path of Lottie Animation JSON File
       skills: [
         emoji(
-          '⚡ Experience in developing Smart Contract using Solidity & Ethereum'
-        ),
+          '⚡ Conducted research and developed an employee satisfaction survey for a large organization, utilizing advanced statistical analysis techniques such as factor analysis and item response theory to gain deep insights into employee engagement and satisfaction.'),
         emoji(
-          '⚡ Building Scripts for automated testing & deployment of Smart Contracts using Brownie & Infura'
-        ),
-        emoji(
-          '⚡ Experience of using Openzeppelin Smart Contract Standards & Chainlink Oracles'
-        ),
-        emoji('⚡ Developing NFT Smart Contracts using ERC-721 Token Standard'),
-        emoji(
-          '⚡ Building Dapps with React.js & Solidity using Web3.js, Moralis & IPFS'
-        ),
+          '⚡ Developed analysis tools in Python to streamline data processing and improve accuracy, resulting in a 20% increase in employee satisfaction and retention.'),
       ],
       softwareSkills: [
         {
-          skillName: 'Ethereum',
-          fontAwesomeClassName: 'logos:ethereum',
+          skillName: 'SPSS',
+          fontAwesomeClassName: 'carbon:ibm-cloud-pak-manta-automated-data-lineage',
         },
         {
-          skillName: 'Solidity',
-          fontAwesomeClassName: 'logos:solidity',
+          skillName: 'Slack',
+          fontAwesomeClassName: 'logos:slack-icon',
         },
         {
-          skillName: 'Web3js',
-          fontAwesomeClassName: 'logos:web3js',
+          skillName: 'Asana',
+          fontAwesomeClassName: 'logos:asana-icon',
         },
         {
-          skillName: 'Metamask',
-          fontAwesomeClassName: 'logos:metamask-icon',
+          skillName: 'Measurement',
+          fontAwesomeClassName: 'gis:measure-line',
         },
         {
-          skillName: 'Ganache',
-          fontAwesomeClassName: 'logos:ganache-icon',
+          skillName: 'SurveyMonkey',
+          fontAwesomeClassName: 'simple-icons:surveymonkey',
+        },
+        {
+          skillName: 'Python',
+          fontAwesomeClassName: 'logos:python',
+        },
+        {
+          skillName: 'MatLab',
+          fontAwesomeClassName: 'vscode-icons:file-type-matlab',
+        },
+      ],
+    },
+    {
+      title: 'Lesson Plan Development',
+      lottieAnimationFile: '/lottie/skills/EmployeSatSurvey.json', // Path of Lottie Animation JSON File
+      skills: [
+        emoji(
+          '⚡ •	Simplified and visualized complex mathematical concepts such as abstract algebra, graph theory, and logic for high school students, incorporating real-world applications and interactive activities to make learning engaging and fun.'),
+      ],
+      softwareSkills: [
+        {
+          skillName: 'Tennis',
+          fontAwesomeClassName: 'emojione:tennis',
+        },
+        {
+          skillName: 'Effective Communication',
+          fontAwesomeClassName: 'icon-park:communication',
+        },
+        {
+          skillName: 'Measureable Results',
+          fontAwesomeClassName: 'tabler:ruler-measure',
         },
       ],
     },
@@ -233,14 +210,6 @@ export const experience: ExperienceType[] = [
     companyLogo: '/img/icons/common/ParqaLogo.png',
     date: 'Apr 2022 – Jun 2022',
     desc: 'As a client-facing data scientist in a marketing agency startup that works with staffing firms, I analyze large amounts of staffing industry data to develop predictive models and segmentation strategies that optimize the performance of clients\' marketing campaigns and recruitment initiatives. I collaborate closely with clients to understand their business goals and requirements, and regularly communicate data-driven insights and recommendations to ensure they stay informed and achieve measurable results.',
-    descBullets: [ 
-      'Developed and implemented a highly effective machine learning algorithm using Python that matched jobs with applicants, resulting in a 200% increase in candidate engagement and leading to more successful matches while significantly improving recruiter efficiency.',
-      'Collaborated with C-suite executives to align business goals and priorities, leveraging advanced statistical analysis and data visualization techniques to drive informed decision-making and foster a culture of data-driven innovation.',
-      'Led development of marketing measurement plans in close partnership with enterprise marketing leaders, utilizing SQL and Salesforce to accurately track and optimize campaigns that ultimately drove a 10% increase in customer acquisition.',
-      'Worked cross-functionally with strategic teams to deliver prioritized marketing strategies with estimated outcomes, leveraging A/B testing, predictive modeling, and data mining techniques.',
-      'Effectively visualized complex data insights using packages such as Matplotlib and Seaborn to communicate key findings and recommendations to business stakeholders, enabling informed decision-making and driving business impact.',
-      'Developed a comprehensive measurement strategy solution that\'s now standard in the first sale SOW, generating $30,000 per client.',
-    ]
   },
   {
     role: 'Tennis Professional',
@@ -276,14 +245,14 @@ export const projects: ProjectType[] = [
 
 export const feedbacks: FeedbackType[] = [
   {
-    name: 'John Smith',
+    name: 'Ty\'s Mom',
     feedback:
-      '- Oh Ty? He rocks',
+      '- He\'s cool and knows data science.',
   },
   {
-    name: 'John Smith',
+    name: 'Amy Vaughan',
     feedback:
-      '- I second the above comment',
+      '- He aight',
   },
 ];
 
@@ -292,19 +261,18 @@ export const showContactUs: boolean = true;
 
 // See object prototype on /types/section.ts page
 export const seoData: SEODataType = {
-  title: 'Hanzla Tauqeer',
+  title: 'Ty Gossett',
   description:
-    'A passionate Full Stack Web Developer and Blockchain Developer.',
-  author: 'Hanzla Tauqeer',
+    'A portfolio site of Data Scientist Ty Gossett',
+  author: 'Ty Gossett',
   image: 'https://avatars3.githubusercontent.com/u/59178380?v=4',
-  url: 'https://developer-portfolio-1hanzla100.vercel.app',
+  url: 'https://ty-portfolio-cfp94mtzq-twg2767-ritgit-page.vercel.app/',
   keywords: [
-    'Hanzla',
-    'Hanzla Tauqeer',
-    '@1hanzla100',
-    '1hanzla100',
+    'Ty',
+    'Ty Gossett',
+    'GossettTy',
     'Portfolio',
-    'Hanzla Portfolio ',
-    'Hanzla Tauqeer Portfolio',
+    'Ty Portfolio ',
+    'Ty Gossett Portfolio',
   ],
 };
