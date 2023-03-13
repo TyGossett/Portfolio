faa2733d8c65fa38c047c35061afea0a534b8b4d
Ty Gossett <34064388+TyGossett@users.noreply.github.com>
2023-03-13 00:54:04 -0400

    reorg

--- portfolio.ts
+++ portfolio.ts
@@ -33,6 +33,25 @@ export const socialLinks = {
   instagram: 'https://www.instagram.com/gossettty/',
 };
 
+export const projects: ProjectType[] = [
+  {
+    name: 'Job Scoring',
+    desc: 'Collaborated with business leaders of a top ten travel nursing and medical staffing agency to assess their tech stack capabilities and develop a high-performing job scoring algorithm using Python and Salesforce data. The algorithm aimed to match candidates with relevant contracts and exceeded traditional matching methods.',
+  },
+  {
+    name: 'Data Strategy',
+    desc: 'Developed and implemented a comprehensive data strategy for a New York-based medical staffing firm, collaborating closely with stakeholders to ensure data privacy, integrity, and quality. The cross-functional initiative included implementing a data model that better aligned the firm\'s database with their ongoing business and marketing objectives.',
+  },
+  {
+    name: 'Employee Satisfaction Survey',
+    desc: 'Conducted research and utilized advanced statistical analysis techniques, such as factor analysis and item response theory, to develop an employee satisfaction survey for a large organization. Developed analysis tools in Python to streamline data processing and improve accuracy, resulting in a 20% increase in employee satisfaction and retention.',
+  },
+  {
+    name: 'Lesson Plan Development',
+    desc: 'Simplified and visualized complex mathematical concepts such as abstract algebra, graph theory, and logic for high school students, incorporating real-world applications and interactive activities to make learning engaging and fun. ',
+  },
+];
+
 export const skillsSection: SkillsSectionType = {
   title: 'Some of my projects',
   subTitle: 'Data Scientist with a passion for work that helps people.',
@@ -146,7 +165,7 @@ export const skillsSection: SkillsSectionType = {
     },
     {
       title: 'Lesson Plan Development',
-      lottieAnimationFile: '/lottie/skills/EmployeSatSurvey.json', // Path of Lottie Animation JSON File
+      lottieAnimationFile: '/lottie/skills/LessonPlan.json', // Path of Lottie Animation JSON File
       skills: [
         emoji(
           '⚡ •	Simplified and visualized complex mathematical concepts such as abstract algebra, graph theory, and logic for high school students, incorporating real-world applications and interactive activities to make learning engaging and fun.'),
@@ -224,24 +243,6 @@ export const experience: ExperienceType[] = [
   },
 ];
 
-export const projects: ProjectType[] = [
-  {
-    name: 'Job Scoring',
-    desc: 'Collaborated with business leaders of a top ten travel nursing and medical staffing agency to assess their tech stack capabilities and develop a high-performing job scoring algorithm using Python and Salesforce data. The algorithm aimed to match candidates with relevant contracts and exceeded traditional matching methods.',
-  },
-  {
-    name: 'Data Strategy',
-    desc: 'Developed and implemented a comprehensive data strategy for a New York-based medical staffing firm, collaborating closely with stakeholders to ensure data privacy, integrity, and quality. The cross-functional initiative included implementing a data model that better aligned the firm\'s database with their ongoing business and marketing objectives.',
-  },
-  {
-    name: 'Employee Satisfaction Survey',
-    desc: 'Conducted research and utilized advanced statistical analysis techniques, such as factor analysis and item response theory, to develop an employee satisfaction survey for a large organization. Developed analysis tools in Python to streamline data processing and improve accuracy, resulting in a 20% increase in employee satisfaction and retention.',
-  },
-  {
-    name: 'Lesson Plan Development',
-    desc: 'Simplified and visualized complex mathematical concepts such as abstract algebra, graph theory, and logic for high school students, incorporating real-world applications and interactive activities to make learning engaging and fun. ',
-  },
-];
 
 export const feedbacks: FeedbackType[] = [
   {
