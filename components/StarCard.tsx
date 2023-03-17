92d8067818c074a66ef29cf3dace2203f89ac032
Ty Gossett <34064388+TyGossett@users.noreply.github.com>
2023-03-16 22:34:06 -0400

    New commit who dis

--- components/StarCard.tsx
+++ components/StarCard.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Card, CardBody } from "reactstrap";
 import './StarRating.css';
 import Fade from 'react-reveal/Fade';
-import { StarBox } from '../types/sections';
+import { StarQuestion } from '../types/sections';
 
 const StarCard = ({ question, onRatingChange }: 
 StarQuestion) => {
