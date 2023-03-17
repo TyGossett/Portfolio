8214738be2222a5d3325f12ec27fc150886b3177
Ty Gossett <34064388+TyGossett@users.noreply.github.com>
2023-03-16 23:27:45 -0400

    Misspelled react

--- containers/StarRating.tsx
+++ containers/StarRating.tsx
@@ -1,4 +1,4 @@
-import Reach, { useState} from 'react';
+import React, { useState} from 'react';
 import {Container, Row, Col}  from 'reactstrap';
 import StarCard from '../components/StarCard';
 import { StarBox } from '../portfolio';
