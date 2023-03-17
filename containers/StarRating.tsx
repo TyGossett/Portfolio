eb3b978ed4f76220c71fb434f454213a863f6ef2
Ty Gossett <34064388+TyGossett@users.noreply.github.com>
2023-03-16 22:18:21 -0400

    It'll work this time

--- containers/StarRating.tsx
+++ containers/StarRating.tsx
@@ -18,7 +18,7 @@ const StarRating = () => {
             </div>
           </div>
           <Row className="row-grid align-items-center">
-            {questions.map((question, index) => {
+            {StarCard.map((question, index) => {
               return (
                 <Col className="order-lg-1" lg="6" key={index}>
                   <StarRating question={question} />
